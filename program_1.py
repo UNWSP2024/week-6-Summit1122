beeaa52a159d1bc33dcbd9dc67c48f1e1a758958
Summit1122 <155012691+Summit1122@users.noreply.github.com>
2024-10-11 14:15:38 -0500

    final submission
    version 1

--- program_1.py
+++ program_1.py
@@ -2,14 +2,29 @@
 # Program #1: Random Dice
 # Write a "randDice" function (with no input) that randomly chooses two numbers between 1 and 6 (inclusive) and then adds them (this is to simulate the rolling of 2 dice).  
 # The dice sum will be the output of this function.
+import random
 
 def randDice():
     # Write your logic to generate 2 numbers between 1 and 6 here
-
+    MIN = 1
+    MAX = 6
+    num_1 = random.randint(MIN, MAX)
+    num_2 = random.randint(MIN, MAX)
     # Sum 2 numbers
-
+    sum = num_1 + num_2
     # return sum to calling function
-
+    return sum
 #########
 # Then write a mainline that calls the "randDice" function 100 times in a for loop.  
 # The mainline then prints the average of the 100 rolls, rounded to the nearest 0.01.
+def main():
+    total = 0
+    for i in range(1,101):
+        number_roll = randDice()
+        total =  total + number_roll
+
+    average = total/100
+    print("This program will show the average of a 100 dice rolls")
+    print(f"The average after 100 rolls was: {average}")
+
+main()
--- program_3.py
+++ program_3.py
@@ -9,3 +9,23 @@
 # The amount of state sales tax.
 # The total sales tax (county plus state)
 # Use at least one function with input and output in this program
+county_tax = 0.25
+state_tax = 0.5
+county_sales_tax = 0
+state_sales_tax = 0
+sales_total = 0
+
+
+
+def main():
+    print("Sales tax calculator")
+    sales_total = float(input("What was your total sales this month?"))
+    county_sales_tax = sales_total * county_tax
+    state_sales_tax = sales_total * state_tax
+    total_sales_tax = county_sales_tax + state_sales_tax
+    total_tax = round(total_sales_tax, 2)
+    print(f"County Sales Tax: {county_sales_tax}")
+    print(f"State Sales Tax: {state_sales_tax}")
+    print(f"Total Sales Tax: {total_sales_tax}")
+
+main()
